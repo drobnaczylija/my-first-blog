39a059880c68610739eedd79a9d962df84ad2691
drobnaczylija <paulinawiejak91@gmail.com>
2016-02-27 12:11:25 +0100

    My Django Girls app, first commit

---
+++ py1.py
@@ -0,0 +1,45 @@
+print("Hello, DjangoGirls!")
+if 3 > 2:
+	print("To działa!")
+elif 5 > 2:
+	print("5 jest większe od 2")
+else:
+ print("5 nie jest większe od 2")
+name = 'Paulina'
+if name == 'Paulina':
+	print('Hej Paulina!')
+elif name == 'Paulina':
+	print('Hej Paulina')
+else:
+	print('Hej nieznajoma!')
+glosnosc = 57
+volume = glosnosc
+if glosnosc < 20:
+	print("prawie nic nie słychać")
+elif 20 <= volume < 40:
+	print("O, muzyka leci w tle.")
+elif 40 <= volume < 60:
+	print("Idealnie, mogę usłyszzeć wszystkie detale")
+elif 60<= volume < 80:
+	print("Dobre na imprezy")
+elif 80<= volume <100:
+	print("Troszku za głośno")
+else:
+	print("Ojoj! Moje uszy!")
+glosnosc = 92
+volume = glosnosc
+if glosnosc < 20:
+	print("prawie nic nie słychać")
+elif 20 <= volume < 40:
+	print("O, muzyka leci w tle.")
+elif 40 <= volume < 60:
+	print("Idealnie, mogę usłyszzeć wszystkie detale")
+elif 60<= volume < 80:
+	print("Dobre na imprezy")
+elif 80<= volume <100:
+	print("Troszku za głośno")
+else:
+	print("Ojoj! Moje uszy!")
+
+if (True):
+ print('true')
---
+++ py2.py
@@ -0,0 +1,31 @@
+def hej():
+	print('Hej!')
+	print('Jak się masz?')
+hej()
+def hej(imie):
+	if imie == 'Paulina':
+		print('Hej Paulina!')
+	elif imie == 'Ania':
+		print('Hej Ania')
+	else:
+		print('Hej nieznajoma!')
+hej("Magda")
+def hej(imie):
+	if imie == 'Paulina':
+		print('Hej Paulina!')
+	elif imie == 'Ania':
+		print('Hej Ania')
+	else:
+		print('Hej nieznajoma!')
+hej("Paulina")
+def hej(imie):
+	if imie == 'Paulina':
+		print('Hej Paulina!')
+	elif imie == 'Ania':
+		print('Hej Ania')
+	else:
+		print('Hej nieznajoma!')
+hej("Ania")
+def hej(imie):
+	print('Hej' + imie + '!')
+hej("Rachel")
